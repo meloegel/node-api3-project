4265a2b476113d5c236366d3d5f15acbf3ab7877
meloegel <meloegel@gmail.com>
2020-07-09 14:26:55 -0400

    added message

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ server.use('/api/posts', logger, postRouter)
 server.use('/api/users', logger, userRouter)
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  const message = process.env.MESSAGE
+  res.status(200).json({ message });
 });
 
 //custom middleware
