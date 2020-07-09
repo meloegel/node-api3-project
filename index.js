69dfa25002d48da457419ab7603c73609d5b35e1
meloegel <meloegel@gmail.com>
2020-07-09 14:25:35 -0400

    for deployment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+
 const server = require('./server')
 
-server.listen(8000, () => {
-    console.log('\n* Server Running on http://localhost:8000 *\n');
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log(`\n* Server Running on http://localhost:${port} *\n`);
 });
+
+// const server = require('./server') //pre depoloyment
+
+// server.listen(8000, () => {
+//     console.log('\n* Server Running on http://localhost:8000 *\n');
+// });
